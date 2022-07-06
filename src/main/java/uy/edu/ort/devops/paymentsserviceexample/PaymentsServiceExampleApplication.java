2af5e62bc8e30df47c6eb6aafea6f68b7c38fbcf
ErnestoAzziorri2021 <ernestoazziorri@gmail.com>
2022-07-06 00:03:00 -0300

    test
    test

--- src/main/java/uy/edu/ort/devops/paymentsserviceexample/PaymentsServiceExampleApplication.java
+++ src/main/java/uy/edu/ort/devops/paymentsserviceexample/PaymentsServiceExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaymentsServiceExampleApplication {
 
 	public static void main(String[] args) {
+		// test 01
 		SpringApplication.run(PaymentsServiceExampleApplication.class, args);
 	}
 
