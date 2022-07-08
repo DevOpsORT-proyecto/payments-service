37b43e5e322b477a875144f775ba17c09d467e23
ErnestoAzziorri2021 <ernestoazziorri@gmail.com>
2022-07-07 22:00:25 -0300

    Dockerfile
    cambios en dockerfile

--- src/main/java/uy/edu/ort/devops/paymentsserviceexample/PaymentsServiceExampleApplication.java
+++ src/main/java/uy/edu/ort/devops/paymentsserviceexample/PaymentsServiceExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaymentsServiceExampleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Payments Url: " + args[0]);
 		SpringApplication.run(PaymentsServiceExampleApplication.class, args);
 	}
 
