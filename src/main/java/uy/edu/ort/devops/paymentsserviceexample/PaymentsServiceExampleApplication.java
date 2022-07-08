0138c2d21635a5037e17c9abbd73990792af5d72
Enzo Cesco <enzo.cesco@gmail.com>
2022-07-07 22:53:20 -0300

    fix System.out que no anda

--- src/main/java/uy/edu/ort/devops/paymentsserviceexample/PaymentsServiceExampleApplication.java
+++ src/main/java/uy/edu/ort/devops/paymentsserviceexample/PaymentsServiceExampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaymentsServiceExampleApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Payments Url: " + args[0]);
+		System.out.println("Payments Service Starting on Port 8080");
 		SpringApplication.run(PaymentsServiceExampleApplication.class, args);
 	}
 
