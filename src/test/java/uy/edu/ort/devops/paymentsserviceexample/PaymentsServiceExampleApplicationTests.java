3b276223e227b4f67c553251ad4bc5e5cfd13c22
ErnestoAzziorri2021 <ernestoazziorri@gmail.com>
2022-07-19 08:53:25 -0300

    Se agregan test unitarios
    Se agregan test unitarios

--- src/test/java/uy/edu/ort/devops/paymentsserviceexample/PaymentsServiceExampleApplicationTests.java
+++ src/test/java/uy/edu/ort/devops/paymentsserviceexample/PaymentsServiceExampleApplicationTests.java
@@ -1,13 +1,31 @@
 package uy.edu.ort.devops.paymentsserviceexample;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import uy.edu.ort.devops.paymentsserviceexample.domain.PaymentStatus;
+import uy.edu.ort.devops.paymentsserviceexample.logic.PaymentsLogic;
 
 @SpringBootTest
 class PaymentsServiceExampleApplicationTests {
 
+	PaymentsLogic paymentsLogic = new PaymentsLogic();
+	PaymentStatus paymentStatus = new PaymentStatus("1122",true,"Done.");
+
 	@Test
-	void contextLoads() {
+	void paymentsLogicTest() {
+		
+		assertEquals("Done.", paymentsLogic.pay("11",true).getDescription() );
+		assertEquals("No money.", paymentsLogic.pay("123",false).getDescription() );
 	}
+
+	@Test
+	void paymentStatusTest() {
+		assertEquals("Done.",paymentStatus.getDescription() );
+		assertEquals("1122", paymentStatus.getOrderId() );
+	}
+
 
 }
